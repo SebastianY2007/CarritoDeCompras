8cc3c04e135f20eee1f64bd91597511dfa522be2
Sebastian <derlis567y@gmail.com>
2025-07-05 21:27:42 -0500

    FEAT(UPS-030): "Actualizacion de clases para un mejor manejo de carritos de compras"

--- src/main/java/ec/edu/ups/dao/impl/CarritoDAOMemoria.java
+++ src/main/java/ec/edu/ups/dao/impl/CarritoDAOMemoria.java
@@ -2,57 +2,52 @@ package ec.edu.ups.dao.impl;
 
 import ec.edu.ups.dao.CarritoDAO;
 import ec.edu.ups.modelo.Carrito;
+import ec.edu.ups.modelo.Usuario;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CarritoDAOMemoria implements CarritoDAO {
-
-    private List<Carrito> carritos;
+    private final Map<Integer, Carrito> carritosMap;
+    private int proximoCodigo = 1;
 
     public CarritoDAOMemoria() {
-        this.carritos = new ArrayList<Carrito>();
+        this.carritosMap = new HashMap<>();
     }
 
     @Override
     public void crear(Carrito carrito) {
-        carritos.add(carrito);
+        carrito.setCodigo(proximoCodigo++);
+        carritosMap.put(carrito.getCodigo(), carrito);
     }
 
     @Override
-    public Carrito buscarPorCodigo(int codigo) {
-        for (Carrito carrito : carritos) {
-            if (carrito.getCodigo() == codigo) {
-                return carrito;
-            }
-        }
-        return null;
+    public Carrito leer(int codigo) {
+        return carritosMap.get(codigo);
     }
 
     @Override
     public void actualizar(Carrito carrito) {
-        for (int i = 0; i < carritos.size(); i++) {
-            if (carritos.get(i).getCodigo() == carrito.getCodigo()) {
-                carritos.set(i, carrito);
-                break;
-            }
+        if (carritosMap.containsKey(carrito.getCodigo())) {
+            carritosMap.put(carrito.getCodigo(), carrito);
         }
     }
 
     @Override
     public void eliminar(int codigo) {
-        Iterator<Carrito> iterator = carritos.iterator();
-        while (iterator.hasNext()) {
-            Carrito carrito = iterator.next();
-            if (carrito.getCodigo() == codigo) {
-                iterator.remove();
-            }
-        }
+        carritosMap.remove(codigo);
     }
 
     @Override
-    public List<Carrito> listarTodos() {
-        return carritos;
+    public List<Carrito> obtenerTodos() {
+        return new ArrayList<>(carritosMap.values());
+    }
+
+    @Override
+    public List<Carrito> buscarPorUsuario(Usuario usuario) {
+        return carritosMap.values().stream().filter(carrito -> carrito.getUsuario().getUsername().equals(usuario.getUsername())).collect(Collectors.toList());
     }
 }
